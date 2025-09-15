a83c106ecf5ad0e31879024eb2692a23420a2b7f
Yoepee <kdy2975@naver.com>
2025-09-16 01:58:17 +0900

    테스트 케이스 apiKey로 작성

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -1,5 +1,7 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.service.MemberService;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import org.hamcrest.Matchers;
@@ -30,12 +32,17 @@ public class ApiV1PostControllerTest {
 
     @Autowired
     private PostService postService;
+    @Autowired
+    private MemberService memberService;
 
     @Test
     @DisplayName("글 쓰기")
     void t1() throws Exception {
+        Member member = memberService.findByUsername("user1").get();
+
+        String authorApiKey = member.getApiKey();
         ResultActions resultActions = mvc.perform(
-                post("/api/v1/posts")
+                post("/api/v1/posts?apiKey=%s".formatted(authorApiKey))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
                                 {
