cc4e51e4fe884a7ef0f7fb30afbd9de2cc539531
Yoepee <kdy2975@naver.com>
2025-09-22 12:51:37 +0900

    계정 인증 방식 변경

--- src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
@@ -39,10 +39,8 @@ public class ApiV1MemberController {
     public RsData<MemberLoginResBody> login(@Valid @RequestBody MemberLoginReqBody reqBody) {
         Member member = memberService.findByUsername(reqBody.username())
                 .orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 회원입니다."));
-
-        if (!member.getPassword().equals(reqBody.password())) {
-            throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
-        }
+        
+        memberService.checkPassword(member, reqBody.password());
 
         String accessToken = memberService.genAccessToken(member);
 
